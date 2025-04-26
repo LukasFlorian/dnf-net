ac12849cb4f33e92107c702da4381a2039df71aa
LukasFlorian <Lukas.richter.1@gmx.net>
2025-04-26 21:09:43 +0200

    doku

--- documentation/convert.py
+++
@@ -1,9 +0,0 @@
-from spire.doc import *
-from spire.doc.common import *
-
-# Load the Markdown file
-doc = Document()
-doc.LoadFromFile("your_markdown_file.md")
-
-# Save as PDF
-doc.SaveToFile("output.pdf", FileFormat.PDF)
